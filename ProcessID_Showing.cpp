ecac6a6fcc0c6180c0b195f1f5d4f30f40cd7dde
Md Nazmul Hasan <nazmul7762@gmail.com>
2025-04-16 14:25:05 +0600

    get processor no

--- ProcessID_Showing.cpp
+++ ProcessID_Showing.cpp
@@ -1,19 +1,22 @@
+#define GNU _GNU_SOURCE
 #include <iostream>
 #include <unistd.h>  // For getpid() and getppid()
 using namespace std;
 int main() {
-    // Get process ID
+    
     int child_status;
     int child_pid;
     int parentPID;
-    
+    int process_no;
+
+    // Get process ID
     parentPID = getpid();
 
     child_status = fork();
     if (child_status==0){
         child_pid=getpid();
-
-        cout << "Parent PID: " << parentPID << ", Child PID: " << child_pid << endl;
+        process_no=sched_getcpu(); // get processor no
+        cout << "Parent PID: " << parentPID << ", Child PID: " << child_pid << ", Processor: " << process_no << endl;
 
     }
     else{
